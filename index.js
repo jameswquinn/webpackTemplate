a39c01454e0059f5ac29669d3ace52ee3ea915d3
jameswquinn <jameswquinn@users.noreply.github.com>
2017-11-13 11:15:35 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import modernizr from './helpers/modernizr.custom'
+import Milligram from 'milligram';
+import Moon from 'moonjs';
+import MoonRouter from 'moon-router';
+import Monx from 'monx';
+import Axios from 'axios';
+import Lazysizes from 'lazysizes';
+import 'lazysizes/plugins/bgset/ls.bgset';
+import _ from './helpers/lodash.custom';
+import Siema from 'siema';
+import Macy from 'macy';
+import CoverVid from 'covervid';
+import Instafeed from 'instafeed';
+//typical import
+//import {TweenMax, Power2, TimelineLite} from "gsap";
+new Siema({
+  perPage: 2
+});
